1791894171f7555774fde0d970a4c941db9e7d77
feichay10 <alu0101364544@ull.edu.es>
2024-03-08 09:22:38 +0000

    Starting doing functions

---
+++ Practica_4_Busqueda_por_dispersion/include/functions.h
@@ -0,0 +1,49 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 4: Búsqueda por dispersión
+ * @file functions.h
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <thread>
+
+const std::string kRedBold = "\033[1;31m";
+const std::string kGreenBold = "\033[1;32m";
+const std::string kBold = "\033[1m";
+const std::string kReset = "\033[0m";
+
+const std::string kUsage =
+    "Usage: ./dispersionSearch -ts <s> -fd <f> -hash <open|close> -bs <s> -fe "
+    "<f>"
+    "\n\nOptions:"
+    "\n  -ts <s>  Size of the table."
+    "\n  -fd <f>  Code that identifies a dispersion function."
+    "\n  -hash <open|close>  Indicates the dispersion technique to use."
+    "\n  -bs <s>  Size of the block. Only for closed dispersion."
+    "\n  -fe <f>  Code that identifies an exploration function. Only for "
+    "closed dispersion."
+    "\n  -help  Show this message.";
+
+void checkFile(std::string fileIn);
+void textMenu();
+
+#endif  // FUNCTIONS_H_
---
+++ Practica_4_Busqueda_por_dispersion/src/functions.cc
@@ -0,0 +1,19 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 4: Búsqueda por dispersión
+ * @file functions.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "../include/functions.h"
