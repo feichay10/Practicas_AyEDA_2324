edb44456d120ead746986cf2aad332a3350a7f17
feichay10 <alu0101364544@ull.edu.es>
2024-03-22 18:58:59 +0000

    Modifying kUsage

--- Practica_5_Algoritmos_de_Ordenacion/include/functions.h
+++ Practica_5_Algoritmos_de_Ordenacion/include/functions.h
@@ -31,22 +31,22 @@
 
 const std::string kUsage =
     BOLD + std::string("NAME\n") + RESET +
-    std::string("       dispersionSearch - Dispersed Search\n\n") + BOLD + std::string("SYNOPSIS\n") +
+    std::string("       sortingAlgorithm - Sorting algorithms\n\n") + BOLD + std::string("SYNOPSIS\n") +
     RESET +
-    std::string("       ./dispersionSearch -ts <s> -fd <f> -hash <open|close> -bs <s> -fe <f>\n\n") +
+    std::string("       ./bin/sortingAlgorithm -size <s> -ord <m> -init <i> -trace <y|n>\n\n") +
     BOLD + std::string("DESCRIPTION\n") + RESET +
-    std::string("       Program that performs a dispersed search in a hash table.\n\n") +
-    BOLD + std::string("OPCIONES") + RESET + BOLD + std::string("\n\t  -ts <s>") + RESET +
-    std::string("\n\t\tSize of the table.\n") + BOLD +
-    std::string("\n\t  -fd <f>, f = module|sum|random") + RESET +
-    std::string("\n\t\tCode that identifies a dispersion function. If dispersion function is not \n\t\tprovided, the default will be the modulus function\n") +
-    BOLD + std::string("\n\t  -hash <open|close>") + RESET +
-    std::string("\n\t\tIndicates the dispersion technique to use. If dispersion technique is not \n\t\tprovided, the default will be the open.\n") +
-    BOLD + std::string("\n\t  -bs <s>") + RESET +
-    std::string("\n\t\tSize of the block. Only for closed dispersion.\n") + BOLD +
-    std::string("\n\t  -fe <f>, f = lineal|quadratic|double|redispersion [fd]") + RESET +
-    std::string( "\n\t\tCode that identifies an exploration function. Only for closed dispersion. \n\t\tIf explotration function is not provided, the default will be the lineal \n\t\tfunction.\n") +
-    std::string("\n\t\tIf redispersion functions is provided, in the next parameters it must \n\t\tprovided a avalaible dispersion function") +
+    std::string("       Program that allows to sort data a sequence in a specific order.\n\n") +
+    BOLD + std::string("OPTIONS") + RESET + BOLD + std::string("\n\t  -size <s>") + RESET +
+    std::string("\n\t\tSize of the sequence.\n") + BOLD +
+    std::string("\n\t  -ord <m>, m = selection|quick|heap|shell|radix") + RESET +
+    std::string("\n\t\tm is de code who identifies the sort method.\n") +
+    BOLD + std::string("\n\t  -init <i> [f]") + RESET +
+    std::string("\n\t\ti = manual. Enter sequence data manually") +
+    std::string("\n\t\ti = random. Enter sequence data randomly") +
+    std::string("\n\t\ti = manual. Enter sequence data by file") +
+    std::string("\n\t\t    f = enter file name\n") +
+    BOLD + std::string("\n\t  -trace <y|n>") + RESET +
+    std::string("\n\t\tIndicates whether or not the trace is displayed during execution\n") + BOLD +
     BOLD + std::string("\n\t  -help") + RESET + std::string("\n\t\tShow this message.");
 
 using keyType = Nif;
@@ -59,9 +59,10 @@ struct sortParameters {
   bool trace_;
 };
 
-// Crear una lista de los algoritmos de ordenacion disponibles
+// Lsta de los algoritmos de ordenacion disponibles
 const std::set<std::string> kAvailableOrders = {"selection", "quick", "heap", "shell", "radix"};
 
 sortParameters checkProgramParameters(int argc, char* argv[], sortParameters parameters);
+void createSequence(sortParameters parameters);
 
 #endif  // FUNCTIONS_H_
