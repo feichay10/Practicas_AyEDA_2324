6eb487e30246ba2af862df448dc6d2a849680917
feichay10 <alu0101364544@ull.edu.es>
2024-03-29 10:34:10 +0000

    minor fixes

--- Practica_5_Algoritmos_de_Ordenacion/src/nif.cc
+++ Practica_5_Algoritmos_de_Ordenacion/src/nif.cc
@@ -1,10 +1,25 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 5: Algoritmos de Ordenacion
+ * @file nif.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-08
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include "../include/nif.h"
 
 const char letters[] = "TRWAGMYFPDXBNJZSQVHLCKE";
 
 Nif::Nif() {
-  // nif_ = 10000000 + rand() % 1000000;
-
   srand(time(0));
   
   std::random_device rd;
