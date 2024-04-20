a04b5219c1a806c87050d9b0282093306bad1f26
feichay10 <alu0101364544@ull.edu.es>
2024-04-20 21:35:52 +0100

    minor fixes

--- Practica_6_Implementacion_TDA_Arbol/include/ABLevel.h
+++ Practica_6_Implementacion_TDA_Arbol/include/ABLevel.h
@@ -1,12 +1,18 @@
 /**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 6: Implementacion del TDA Árbol 
  * @file ABLevel.h
- * @author your name (you@domain.com)
- * @brief 
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
  * @version 0.1
- * @date 2024-04-15
- * 
- * @copyright Copyright (c) 2024
- * 
+ * @date 2024-04-22
+ *
+ * @copyright Copyright (c) 2023
+ *
  */
 
 #ifndef ABLEVEL_H
--- Practica_6_Implementacion_TDA_Arbol/include/colors.h
+++ Practica_6_Implementacion_TDA_Arbol/include/colors.h
@@ -4,12 +4,12 @@
  * Grado en Ingeniería Informática
  * Asignatura: Algoritmos y Estructura de Datos Avanzada
  * Curso: 2º
- * Práctica 7: Implementacion del TDA AVL
+ * Práctica 6: Implementacion del TDA Árbol 
  * @file colors.h
  * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
  * @brief
  * @version 0.1
- * @date 2024-04-29
+ * @date 2024-04-22
  *
  * @copyright Copyright (c) 2023
  *
