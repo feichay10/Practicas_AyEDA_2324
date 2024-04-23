dacf0777dcafcddbbbb27aec3261e70118bc6638
feichay10 <alu0101364544@ull.edu.es>
2024-04-23 09:32:07 +0100

    Adding possible modification

---
+++ Practica_7_Implementacion_TDA_AVL/Modification_p7/include/AB.h
@@ -0,0 +1,347 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 7: Implementacion del TDA AVL
+ * @file AB.h
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-29
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef AB_H
+#define AB_H
+
+#include <iostream>
+#include <queue>
+
+#include "nodeB.h"
+#include "colors.h"
+
+/**
+ * @brief Class AB that represents a binary tree
+ * 
+ * @tparam Key 
+ */
+template <class Key>
+class AB {
+ public:
+  NodeB<Key>* getRoot() const;
+  NodeB<Key>*& getRoot();
+  void setRoot(NodeB<Key>* root);
+  int getNumNodes() const;
+
+  // Methods
+  virtual bool insert(const Key& k) = 0;
+  virtual bool search(const Key& k) const = 0;
+  virtual bool remove(const Key& k) = 0;
+
+  // Tour methods
+  void inorder() const;
+  void inorder(NodeB<Key>* node) const;
+  void preorder() const;
+  void preorder(NodeB<Key>* node) const;
+  void postorder() const;
+  void postorder(NodeB<Key>* node) const;
+  void byLevel() const;
+  void byLevel(NodeB<Key>* node) const;
+
+  // Other methods
+  int height() const;
+  int heightN(NodeB<Key>* node) const;
+  bool empty();
+  int getLeafCount() const;
+  int countLeafNodes(NodeB<Key>* node) const;
+
+  virtual void write(std::ostream& os) const;
+  friend std::ostream& operator<<(std::ostream& os, const AB<Key>& ab) {
+    ab.write(os);
+    return os;
+  }
+
+  // Modification
+  int getCountSearchesABB() {
+    return countSearchesABB_;
+  }
+
+  int getCountSearchesABE() {
+    return countSearchesABE_;
+  }
+
+  int getCountSearchesAVL() {
+    return countSearchesAVL_;
+  }
+
+ protected:
+  NodeB<Key>* root_;
+  int numNodes_ = 0;
+
+  // Modification
+  mutable int countSearchesABB_ = 0;
+  mutable int countSearchesABE_ = 0;
+  mutable int countSearchesAVL_ = 0;
+};
+
+/**
+ * @brief Get a pointer to the root node of the binary tree.
+ * 
+ * @tparam Key 
+ * @return NodeB<Key>* 
+ */
+template <class Key>
+NodeB<Key>* AB<Key>::getRoot() const {
+  return root_;
+}
+
+/**
+ * @brief Get a reference to a pointer to the root node of the binary tree
+ * 
+ * @tparam Key 
+ * @return NodeB<Key>*& 
+ */
+template <class Key>
+NodeB<Key>*& AB<Key>::getRoot() {
+  return root_;
+}
+
+/**
+ * @brief Set the root node of the binary tree
+ * 
+ * @tparam Key 
+ * @param root 
+ */
+template <class Key>
+void AB<Key>::setRoot(NodeB<Key>* root) {
+  root_ = root;
+}
+
+/**
+ * @brief Get the number of nodes in the binary tree
+ * 
+ * @tparam Key 
+ * @return int 
+ */
+template <class Key>
+int AB<Key>::getNumNodes() const {
+  return numNodes_;
+}
+
+/**
+ * @brief Inorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ */
+template <class Key>
+void AB<Key>::inorder() const {
+  inorder(root_);
+}
+
+/**
+ * @brief Inorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ */
+template <class Key>
+void AB<Key>::inorder(NodeB<Key>* node) const {
+  if (node != nullptr) {
+    inorder(node->getLeft());
+    std::cout << BLUE_BOLD << node->getData() << " " << RESET;
+    inorder(node->getRight());
+  }
+}
+
+/**
+ * @brief Preorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ */
+template <class Key>
+void AB<Key>::preorder() const {
+  preorder(root_);
+}
+
+/**
+ * @brief Preorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ */
+template <class Key>
+void AB<Key>::preorder(NodeB<Key>* node) const {
+  if (node != nullptr) {
+    std::cout << BLUE_BOLD << node->getData() << " " << RESET;
+    preorder(node->getLeft());
+    preorder(node->getRight());
+  }
+}
+
+/**
+ * @brief Postorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ */
+template <class Key>
+void AB<Key>::postorder() const {
+  postorder(root_);
+}
+
+/**
+ * @brief Postorder traversal of the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ */
+template <class Key>
+void AB<Key>::postorder(NodeB<Key>* node) const {
+  if (node != nullptr) {
+    postorder(node->getLeft());
+    postorder(node->getRight());
+    std::cout << BLUE_BOLD << node->getData() << " " << RESET;
+  }
+}
+
+/**
+ * @brief Level order traversal of the binary tree
+ * 
+ * @tparam Key 
+ */
+template <class Key>
+void AB<Key>::byLevel() const {
+  byLevel(root_);
+}
+
+/**
+ * @brief Level order traversal of the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ */
+template <class Key>
+void AB<Key>::byLevel(NodeB<Key>* node) const {
+  std::queue<NodeB<Key>*> queue;
+  queue.push(node);
+  while (!queue.empty()) {
+    if (queue.front() != nullptr) {
+      std::cout << BLUE_BOLD << queue.front()->getData() << " " << RESET;
+      queue.push(queue.front()->getLeft());
+      queue.push(queue.front()->getRight());
+    }
+    queue.pop();
+  }
+}
+
+/**
+ * @brief Get the height of the binary tree
+ * 
+ * @tparam Key 
+ * @return int 
+ */
+template <class Key>
+int AB<Key>::height() const {
+  return heightN(root_);
+}
+
+/**
+ * @brief Get the height of the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ * @return int 
+ */
+template <class Key>
+int AB<Key>::heightN(NodeB<Key>* node) const {
+  if (node == nullptr) {
+    return 0;
+  } else {
+    int leftHeight = heightN(node->getLeft());
+    int rightHeight = heightN(node->getRight());
+
+    if (leftHeight > rightHeight) {
+      return leftHeight + 1;
+    } else {
+      return rightHeight + 1;
+    }
+  }
+}
+
+/**
+ * @brief Check if the binary tree is empty
+ * 
+ * @tparam Key 
+ * @return true 
+ * @return false 
+ */
+template <class Key>
+bool AB<Key>::empty() {
+  return root_ == nullptr;
+}
+
+/**
+ * @brief Get the number of leaf nodes in the binary tree
+ * 
+ * @tparam Key 
+ * @return int 
+ */
+template <class Key>
+int AB<Key>::getLeafCount() const {
+  return countLeafNodes(root_);
+}
+
+/**
+ * @brief Count the number of leaf nodes in the binary tree
+ * 
+ * @tparam Key 
+ * @param node 
+ * @return int 
+ */
+template <class Key>
+int AB<Key>::countLeafNodes(NodeB<Key>* node) const {
+  if (node == nullptr) {
+    return 0;
+  }
+  if (node->getLeft() == nullptr && node->getRight() == nullptr) {
+    return 1;
+  }
+  return countLeafNodes(node->getLeft()) + countLeafNodes(node->getRight());
+}
+
+/**
+ * @brief Write the binary tree to an output stream
+ * 
+ * @tparam Key 
+ * @param os 
+ */
+template <class Key>
+void AB<Key>::write(std::ostream& os) const {
+  int k = 0;
+  std::queue<NodeB<Key>*> queue, queueAux;
+  queue.push(root_);
+  while (!queue.empty()) {
+    os << "Level " << k << ": ";
+    while (!queue.empty()) {
+      if (queue.front() != nullptr) {
+        os << "[" << queue.front()->getData() << "] ";
+        queueAux.push(queue.front()->getLeft());
+        queueAux.push(queue.front()->getRight());
+      } else {
+        os << "[.] ";
+      }
+      queue.pop();
+    }
+    queue = queueAux;
+    while (!queueAux.empty()) {
+      queueAux.pop();
+    }
+    os << std::endl;
+    k++;
+  }
+}
+
+#endif  // AB_H
---
+++ Practica_7_Implementacion_TDA_AVL/Modification_p7/src/functions.cc
@@ -0,0 +1,278 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 7: Implementacion del TDA AVL
+ * @file functions.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-29
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "../include/functions.h"
+
+treeParameters checkProgramParameters(int argc, char* argv[], treeParameters& parameters) {
+  if (argc == 2 && std::string(argv[1]) == "-help") {
+    std::cout << kUsage << std::endl;
+    exit(EXIT_SUCCESS);
+  } else if (argc == 1) {
+    throw std::runtime_error("No arguments provided. Use " + std::string(argv[0]) + " -help for more information.");
+  }
+
+  // Check if tree type is AVL, trace must be provided (-trace option)
+    // Check for tree type and initialization parameters, must be provided
+  bool treeType = false, init = false, traceFlag = false;
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-ab") {
+      treeType = true;
+    } else if (std::string(argv[i]) == "-init") {
+      init = true;
+    } else if (std::string(argv[i]) == "-trace") {
+      traceFlag = true;
+    }
+  }
+
+  if (!treeType) {
+    throw std::invalid_argument("Tree type parameter not provided. Use " + std::string(argv[0]) + " -help for more information.");
+  } else if (!init) {
+    throw std::invalid_argument("Initialization not provided. Use " + std::string(argv[0]) + " -help for more information.");
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-ab") {
+      if (i + 1 < argc) {
+        if (std::string(argv[i + 1]) == "abe" || std::string(argv[i + 1]) == "abb" || std::string(argv[i + 1]) == "avl" ||std::string(argv[i + 1]) == "ab") {
+          if (std::string(argv[i + 1]) == "avl" && !traceFlag) {
+            throw std::invalid_argument("Trace option not provided. Use " + std::string(argv[0]) + " -help for more information.");
+          }
+          parameters.treeType_ = argv[i + 1];
+        } else {
+          throw std::invalid_argument("Tree type " + std::string(argv[i + 1]) + " not avalaible. Use " + std::string(argv[0]) + " -help for more information.");
+        }
+      } else {
+        throw std::invalid_argument("Tree type not provided. Use " + std::string(argv[0]) + " -help for more information.");
+      }
+    } else if (std::string(argv[i]) == "-init") {
+      if (i + 1 < argc) {
+        if (std::string(argv[i + 1]) == "manual" || std::string(argv[i + 1]) == "random") {
+          parameters.init_ = argv[i + 1];
+        } else if (std::string(argv[i + 1]) == "file") {
+          if (i + 2 < argc) {
+            parameters.init_ = argv[i + 1];
+            parameters.file_ = argv[i + 3];
+          } else {
+            throw std::invalid_argument("File not provided. Use " + std::string(argv[0]) + " -help for more information.");
+          }
+        } else {
+          throw std::invalid_argument("Initialization " + std::string(argv[i + 1]) + " not avalaible. Use " + std::string(argv[0]) + " -help for more information.");
+        }
+        if (parameters.init_ == "manual") {
+          parameters.numberGenerated_ = 0;
+        } else {
+          parameters.numberGenerated_ = std::stoi(argv[i + 2]);
+        }
+      } else {
+        throw std::invalid_argument("Initialization not provided. Use " + std::string(argv[0]) + " -help for more information.");
+      }
+    } else if (std::string(argv[i]) == "-trace") {
+      if (i + 1 < argc) {
+        if (std::string(argv[i + 1]) == "y" || std::string(argv[i + 1]) == "Y") {
+          parameters.trace_ = true;
+        } else if (std::string(argv[i + 1]) == "n" || std::string(argv[i + 1]) == "N") {
+          parameters.trace_ = false;
+        } else {
+          throw std::invalid_argument("Option for -trace not available. Use " + std::string(argv[0]) + " -help for more information.");
+        }
+      } else {
+        throw std::invalid_argument("Trace not provided. Use " + std::string(argv[0]) + " -help for more information.");
+      }
+    }
+  }
+
+  return parameters;
+}
+
+void printInformation(treeParameters& parameters) {
+  std::cout << PURPLE_BOLD << "+-----------------------------------------------+" << RESET << std::endl;
+  std::cout << PURPLE_BOLD << "|" << RESET << ORANGE_BG << GRAY_BOLD << "                TDA Information                " << RESET << PURPLE_BOLD << "|" << std::endl;
+  std::cout << PURPLE_BOLD << "+-----------------------------------------------+" << RESET << std::endl;
+  std::cout << PURPLE_BOLD << "|" << RESET << GRAY_BOLD
+            << " Tree type: " << BLUE_BOLD
+            << (parameters.treeType_ == "abe" ? "Balanced binary tree (ABE)" : (parameters.treeType_ == "abb" ? "Binary search tree (ABB)" : "Balanced binary search tree (AVL)"))
+            << PURPLE_BOLD
+            << std::setw(21 - (parameters.treeType_ == "abe" ? 11 : (parameters.treeType_ == "abb" ? 9 : 18)))
+            << "|" << RESET << std::endl;
+  std::cout << PURPLE_BOLD << "|" << RESET << GRAY_BOLD
+            << " Initialization: " << BLUE_BOLD
+            << parameters.init_ << PURPLE_BOLD
+            << std::setw(31 - parameters.init_.length()) << "|"
+            << RESET << std::endl;
+  std::cout << PURPLE_BOLD << "|" << RESET << GRAY_BOLD
+            << " Number of elements: " << BLUE_BOLD
+            << parameters.numberGenerated_ << PURPLE_BOLD
+            << std::setw(27 - std::to_string(parameters.numberGenerated_).length())
+            << "|" << RESET << std::endl;
+  if (parameters.init_ == "file") {
+    std::cout << PURPLE_BOLD << "|" << RESET << GRAY_BOLD
+              << " File: " << BLUE_BOLD << parameters.file_
+              << PURPLE_BOLD
+              << std::setw(41 - parameters.file_.length())
+              << "|" << RESET << std::endl;
+  }
+  if (parameters.treeType_ == "avl") {
+    std::cout << PURPLE_BOLD << "|" << RESET << GRAY_BOLD
+              << " Trace: " << BLUE_BOLD << (parameters.trace_ ? "Yes" : "No")
+              << PURPLE_BOLD
+              << std::setw(40 - (parameters.trace_ ? 3 : 2))
+              << "|" << RESET << std::endl;
+  }
+  std::cout << PURPLE_BOLD << "+-----------------------------------------------+" << RESET << std::endl;
+}
+
+void createTree(treeParameters& parameters) {
+  AB<keyType>* tree;
+  if (parameters.treeType_ == "abe") {
+    tree = new ABE<keyType>();
+  } else if (parameters.treeType_ == "abb") {
+    tree = new ABB<keyType>();
+  } else if (parameters.treeType_ == "avl") {
+    tree = new AVL<keyType>();
+    if (parameters.trace_) {
+      static_cast<AVL<keyType>*>(tree)->setTrace(true);
+    }
+  }
+
+  if (parameters.init_ == "random") {
+    for (int i = 0; i < parameters.numberGenerated_; i++) {
+      keyType key;
+      std::cout << BOLD << "\nInsert key: " << RESET << key << std::endl;
+      tree->insert(key);
+      tree->write(std::cout);
+    }
+  } else if (parameters.init_ == "file") {
+    std::ifstream file(parameters.file_);
+    if (!file.is_open()) {
+      throw std::runtime_error("File could not be opened.");
+    }
+
+    int numElements, count = 0;
+    file >> numElements;
+    if (numElements != parameters.numberGenerated_) {
+      throw std::length_error("Number of elements in file does not match with the number of elements provided.");
+    }
+
+    keyType key;
+    while (file >> key) {
+      std::cout << BOLD << "\nInsert key: " << RESET << key << std::endl;
+      tree->insert(key);
+      count++;
+      tree->write(std::cout);
+    }
+
+    if (count != numElements) {
+      throw std::length_error("Number of elements in file does not match with the number of elements provided.");
+    }
+
+    file.close();
+  }
+
+  std::cout << std::endl;
+  if (tree->empty()) {
+    std::cout << "Tree is empty." << std::endl;
+    tree->write(std::cout);
+  }
+
+  menu(tree, parameters);
+}
+
+void menu(AB<keyType>* tree, treeParameters& parameters) {
+  int option;
+  keyType key;
+
+  while (true) {
+    std::cout << BOLD << "\n\nOperations" << RESET << std::endl;
+    std::cout << RED_BOLD << "  [0]" << RESET << BOLD << " Exit" << std::endl;
+    std::cout << RED_BOLD << "  [1]" << RESET << BOLD << " Insert key" << std::endl;
+    std::cout << RED_BOLD << "  [2]" << RESET << BOLD << " Search key" << std::endl;
+    std::cout << RED_BOLD << "  [3]" << RESET << BOLD << " Delete key" << std::endl;
+    std::cout << RED_BOLD << "  [4]" << RESET << BOLD << " Show inorder tree" << std::endl;
+    std::cout << RED_BOLD << "  [5]" << RESET << BOLD << " Show all properties" << std::endl;
+    std::cout << RED_BOLD << "  [6]" << RESET << BOLD << " Show tree" << std::endl;
+    std::cout << "Select operation: " << RESET; 
+    std::cin >> option;
+
+    switch (option) {
+      case 0:
+        std::cout << BOLD << "\nExiting program..." << RESET << std::endl;
+        exit(EXIT_SUCCESS);
+        break;
+      case 1:
+        std::cout << BOLD << "\nInsert key: " << RESET;
+        std::cin >> key;
+        if (!tree->insert(key)) {
+          std::cout << RED_BOLD << "Key already exists." << RESET << std::endl << std::endl;
+        }
+        tree->write(std::cout);
+        break;
+      case 2:
+        std::cout << BOLD << "\nSearch key: " << RESET;
+        std::cin >> key;
+        if (tree->search(key)) {
+          std::cout << GREEN_BOLD << "Key found." << RESET << std::endl;
+        } else {
+          std::cout << RED_BOLD << "Key not found." << RESET << std::endl;
+        }
+        break;
+      case 3:
+        if (parameters.treeType_ == "abe") {
+          std::cout << RED_BOLD << "\nDelete key operation not available for ABE tree." << RESET << std::endl;
+        } else {
+          std::cout << BOLD << "\nDelete key: " << RESET;
+          std::cin >> key;
+          if (!tree->remove(key)) {
+            std::cout << RED_BOLD << "Key not found." << RESET << std::endl << std::endl;
+          }
+          tree->write(std::cout);
+        }
+        break;
+      case 4:
+        std::cout << BOLD << "\nInorder tree: " << RESET;
+        tree->inorder();
+        std::cout << std::endl;
+        break;
+      case 5:
+        std::cout << BOLD << "\nTree properties: " << RESET << std::endl;
+        std::cout << " -Height: " << BLUE_BOLD << tree->height() << RESET << std::endl;
+        std::cout << " -Number of nodes: " << BLUE_BOLD << tree->getNumNodes() << RESET << std::endl;
+        std::cout << " -Number of leaf nodes: " << BLUE_BOLD << tree->getLeafCount() << RESET << std::endl;
+        std::cout << " -Height of left subtree: " << BLUE_BOLD << tree->heightN(tree->getRoot()->getLeft()) << RESET << std::endl;
+        std::cout << " -Height of right subtree: " << BLUE_BOLD << tree->heightN(tree->getRoot()->getRight()) << RESET << std::endl;
+        std::cout << " -Preorder tree: " << BLUE_BOLD;
+        tree->preorder();
+        std::cout << RESET << std::endl;
+        std::cout << " -Postorder tree: " << BLUE_BOLD;
+        tree->postorder();
+        std::cout << RESET << std::endl;
+        std::cout << " -Inorder tree: " << BLUE_BOLD;
+        tree->inorder();
+        std::cout << RESET << std::endl;
+        std::cout << " -By level tree: " << BLUE_BOLD;
+        tree->byLevel();
+        std::cout << RESET << std::endl;
+        break;
+      case 6:
+        std::cout << BOLD << "\nTree: " << RESET << std::endl;
+        tree->write(std::cout); 
+        break;
+      default:
+        std::cout << RED_BOLD << "\nInvalid option." << RESET << std::endl;
+        std::cout << "Please select a valid option." << std::endl;
+    }
+  }
+}
